4d8ddd42bdf7100991eeb4bf225023ffcbb1b76d
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-02-07 16:19:59 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 import os
-modules = ['async-eval==0.1.11', 'pyrogram==2.0.97', 'qrcode', 'python-telegram-bot==20.0', 'opencv-python', 'yt-dlp', 'requests', 'SafoneAPI', 'dotmap', 'emoji', 'gTTS', 'wget', 'calendar-cli', 'speedtest-cli', 'tinydb==4.7.1', 'Telethon==1.27.0']
-for  in modules:
+
+
+for module in lines[0].split()::
  os.system(module)
