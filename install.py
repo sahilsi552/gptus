63ab8f855f362beca33abacd4b5a4c0efdf02caf
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-02-07 16:13:44 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,4 @@
+import os
+modules = ['async-eval==0.1.11', 'pyrogram==2.0.97', 'qrcode', 'python-telegram-bot==20.0', 'opencv-python', 'yt-dlp', 'requests', 'SafoneAPI', 'dotmap', 'emoji', 'gTTS', 'wget', 'calendar-cli', 'speedtest-cli', 'tinydb==4.7.1', 'Telethon==1.27.0']
+for  in modules:
+ os.system(module)
