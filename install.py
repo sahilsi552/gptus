f71a9804a030b6597d8ba3e9946ff503f32f14e5
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-02-07 16:24:36 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,10 @@
 import os
-
-file = open("requirements.txt","r")
-lines = file.readlines()
-file.close()
-
-for module in lines[0].split():
- os.system(module)
+def install():
+ file = open("requirements.txt","r")
+ lines = file.readlines()
+ file.close()
+ if lines:
+  for module in lines[0].split():
+   os.system(module)
+ else:
+  print("requirements.txt file not found unable to install requirements")
