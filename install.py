0dc8b08534afcb0806016195410a698ea6d91e55
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-02-07 16:44:37 +0530

    Update install.py

--- install.py
+++ install.py
@@ -2,17 +2,18 @@
 
 import os
 def install():
- file = open("requirements.txt","r")
- lines = file.read()
- file.close()
- print()
- if lines:
-  for module in lines.split("\n"):
+ try:
+  file = open("requirements.txt","r")
+  lines = file.read()
+  file.close()
+  print()
+ except:
+  print("requirements.txt file not found unable to install requirements")
+  return
+ for module in lines.split("\n"):
    try:
     os.system("pip3 install " + module)
    except Exception as e:
     print(e)
     continue
- else:
-  print("requirements.txt file not found unable to install requirements")
 install()
